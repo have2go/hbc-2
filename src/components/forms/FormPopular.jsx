3b2bd40ecc2c0a6ec0bd580347c14ada8765503e
have2go <boxadmail@yandex.ru>
2024-04-09 13:36:32 +0300

    added emailjs in popular services form

--- src/components/forms/FormPopular.jsx
+++ src/components/forms/FormPopular.jsx
@@ -1,13 +1,44 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import emailjs from "@emailjs/browser";
 
 export default function FormPopular({ card }) {
+    const PUBLIC_KEY = "o3u4fext32rohw6Zf";
+    const TEMPLATE_ID = "template_sajxblj";
+    const SERVICE_ID = "service_l6aslnc";
+
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [service, setService] = useState(card.title);
     const [phone, setPhone] = useState("");
+    const [message, setMessage] = useState("");
+
+    const templateParams = {
+        from_name: name,
+        form_emai: email,
+        from_phone: phone,
+        from_service: service,
+        from_section: "Популярные услуги",
+    };
+
+    const sendEmail = e => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(SERVICE_ID, TEMPLATE_ID, templateParams, {
+                publicKey: PUBLIC_KEY,
+            })
+            .then(
+                () => {
+                    console.log("SUCCESS!");
+                },
+                error => {
+                    console.log("FAILED...", error.text);
+                }
+            );
+    };
 
     return (
-        <form action="" className="flex flex-col items-center">
+        <form className="flex flex-col items-center" onSubmit={sendEmail}>
             <h4 className="text-3xl">Заказать услугу</h4>
             <p className="text-sm py-2">С Вами свяжется наш менеджер</p>
             <div className="grid grid-cols-2 gap-x-8 gap-y-4 py-4 input-container">
@@ -55,7 +86,16 @@ export default function FormPopular({ card }) {
                 </label>
                 <label htmlFor="" className="col-start-1 col-end-3 flex flex-col">
                     Дополнительная информация
-                    <textarea name="" className="max-h-24 resize-none text-black" id="" cols="30" rows="10"></textarea>
+                    <textarea
+                        name=""
+                        className="max-h-24 resize-none text-black"
+                        id=""
+                        cols="30"
+                        rows="10"
+                        placeholder=""
+                        value={message}
+                        onChange={e => setMessage(e.target.value)}
+                    ></textarea>
                 </label>
             </div>
             <div className="flex gap-2">
